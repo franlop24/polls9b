5dcbc547e95163866c659e0ebce510e0f37adf2e
Franlop24 <franlopbri@gmail.com>
2024-05-13 11:47:44 -0600

    02 Question and Choice Models in Polls

--- pollsapp/pollsapp/settings.py
+++ pollsapp/pollsapp/settings.py
@@ -31,6 +31,9 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    #local Apps
+    'polls.apps.PollsConfig',
+    # django Apps
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
